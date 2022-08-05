c914141c50f63c144233b75e1fb3a81bc85046da
Mahesh Waghmare <waghmarem539@gmail.com>
2022-08-05 03:59:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 import "./App.css";
 
 const App = () => {
+    
+    const [num, setNum] = useState(0);
+    
   return (
     <>
       <div className="main_div">
         <div className="center_div">
-          <h1> 0 </h1>
+          <h1> {num} </h1>
           <div className="btn_div">
             <button>Increm</button>
             <button>Decrem</button>
