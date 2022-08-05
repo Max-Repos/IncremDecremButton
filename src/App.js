e906691a75e0b59cf1fa754be79b03f98799a3e2
Mahesh Waghmare <64427821+Max-Repos@users.noreply.github.com>
2022-08-05 08:40:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1> 0 </h1>
+          <div className="btn_div">
+            <button>Increm</button>
+            <button>Decrem</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
